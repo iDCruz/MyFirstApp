1b90d7852217c3724675622ff6150b69a7427c3e
David Cruz <cruzd@mail.usf.edu>
2015-10-23 11:55:54 -0400

    Init commit

--- app/src/main/java/com/mycompany/myfirstapp/MyActivity.java
+++ app/src/main/java/com/mycompany/myfirstapp/MyActivity.java
@@ -12,7 +12,7 @@ import android.widget.EditText;
 public class MyActivity extends AppCompatActivity {
 
    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
-//comments
+//commentss
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
